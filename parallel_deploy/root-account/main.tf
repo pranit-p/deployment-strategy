bfe30d15f95fd0cf5ad32923a7a4c05188abd756
pranit-p <pranit.pawar@medly.com>
2021-10-11 10:59:15 +0530

    Realise not possible to organization deploy stackset using delegated admin

--- parallel_deploy/root-account/main.tf
+++ parallel_deploy/root-account/main.tf
@@ -1,8 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_organizations_delegated_administrator" "cloudformation_delegated_admin" {
-  account_id        = "251999700057"
-  service_principal = "member.org.stacksets.cloudformation.amazonaws.com"
-}
